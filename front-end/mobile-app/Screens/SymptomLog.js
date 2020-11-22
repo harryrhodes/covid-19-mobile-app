8fb2eeafc225b8d0639a9ee36c9ee6064967baac
Jade Jellicoe <jade@Jades-MacBook-Pro.local>
2020-11-22 14:27:39 +0000

    starting symptom log

--- front-end/mobile-app/Screens/SymptomLog.js
+++ front-end/mobile-app/Screens/SymptomLog.js
@@ -1,15 +1,12 @@
 import React from "react";
 import {SafeAreaView} from "react-native";
-import {Button} from "react-native-paper";
 import {Styles} from "../Styles/HomeStyle"
-import SymptomsCard from "../Components/SymptomsCard/SymptomsCard";
-import BookTestCard from "../Components/BookTestCard/BookTestCard";
-import LogSymptomsCard from "../Components/LogSymptomsCard/LogSymptomsCard";
-import Welcome from "../Components/HomeScreen/Welcome/Welcome";
+import SymptomForm from "../Components/Symptoms/SymptomForm/SymptomForm"
 
 export default function SymptomLog() {
   return (
     <SafeAreaView style={Styles.container}>
+      <SymptomForm />
     </SafeAreaView>
   );
 }
