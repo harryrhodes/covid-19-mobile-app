899c2dd0ff4f53d9c3e34bb248bdba30d8f63db7
Harry <41304347+harryrhodes@users.noreply.github.com>
2020-11-30 12:16:01 +0000

    book test added

--- front-end/mobile-app/Components/BookTestCard/BookTestCard.js
+++ front-end/mobile-app/Components/BookTestCard/BookTestCard.js
@@ -1,21 +1,29 @@
 import React from "react";
 import { View } from "react-native";
 import { ActivityIndicator, Card, Button } from "react-native-paper";
+import {WebView} from "react-native-webview"
 import { Styles } from "./style";
 
 export default function BookTestCard() {
+  const [open, setOpen] = useState(false);
+  const [opened, setOpened] = useState(false);
   return (
     <View style={Styles.fullWidthView}>
       <Card style={Styles.card}>
         <Card.Title title="Book A Test" />
         <Card.Content style={Styles.cardContent}>
-          <Button mode="contained" style={Styles.loginButton}>
+          <Button mode="contained">
             Book a test now
           </Button>
-          <ActivityIndicator
-            animating={false}
-            style={Styles.activityIndicator}
-          />
+        </Card.Content>
+        <Card.Title title="Did you book a test?" />
+        <Card.Content style={Styles.cardContent}>
+          <Button mode="contained">
+            Yes
+          </Button>
+          <Button mode="contained">
+            No
+          </Button>
         </Card.Content>
       </Card>
     </View>
