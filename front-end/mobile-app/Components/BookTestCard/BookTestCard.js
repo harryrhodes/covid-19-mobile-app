ce4596df8cc67cf8ea3baae2af3d1f5f3e14136b
Harry <41304347+harryrhodes@users.noreply.github.com>
2020-11-30 13:09:13 +0000

    Book test functionality added

--- front-end/mobile-app/Components/BookTestCard/BookTestCard.js
+++ front-end/mobile-app/Components/BookTestCard/BookTestCard.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
-import { ActivityIndicator, Card, Button } from "react-native-paper";
-import {WebView} from "react-native-webview"
+import { Subheading, Card, Button } from "react-native-paper";
+import * as WebBrowser from "expo-web-browser";
 import { Styles } from "./style";
 
 export default function BookTestCard() {
   const [open, setOpen] = useState(false);
   const [opened, setOpened] = useState(false);
+
+  const _handlePressButtonAsync = async () => {
+    let result = await WebBrowser.openBrowserAsync(
+      "https://www.gov.uk/get-coronavirus-test"
+    );
+    if (result.type == "opened") {
+      setOpened(true);
+    } else {
+      setOpened(false);
+    }
+  };
+  const handleFeeback = (testBooked) => {
+    if (testBooked == true) {
+      console.log("Booked");
+    } else {
+      console.log("Cancel");
+      setOpened(false);
+    }
+  };
   return (
     <View style={Styles.fullWidthView}>
       <Card style={Styles.card}>
         <Card.Title title="Book A Test" />
-        <Card.Content style={Styles.cardContent}>
-          <Button mode="contained">
-            Book a test now
-          </Button>
-        </Card.Content>
-        <Card.Title title="Did you book a test?" />
-        <Card.Content style={Styles.cardContent}>
-          <Button mode="contained">
-            Yes
-          </Button>
-          <Button mode="contained">
-            No
-          </Button>
-        </Card.Content>
+        {opened == false ? (
+          <Card.Content style={Styles.cardContent}>
+            <Button mode="contained" onPress={_handlePressButtonAsync}>
+              Book a test now
+            </Button>
+          </Card.Content>
+        ) : (
+          <Card.Content style={Styles.cardContent}>
+            <Subheading>Did you book a test?</Subheading>
+            <Button mode="contained" onPress={() => handleFeeback(true)}>
+              Yes
+            </Button>
+            <Button mode="outlined" onPress={() => handleFeeback(false)}>
+              No
+            </Button>
+          </Card.Content>
+        )}
       </Card>
     </View>
   );
