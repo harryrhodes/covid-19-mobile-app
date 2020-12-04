f6e206b7f721c37bc9e8c953aa2fb1716dfd1adf
Robert Kopec <Robert.Kopec@ivanti.com>
2020-12-04 18:30:32 +0000

    remaining tests

---
+++ front-end/admin-panel/src/Tests/Pages/Trends/SymptomTrends.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import SymptomTrends from '../../../Pages/Trends/SymptomTrends';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Chart from "../../../Components/Chart";
+import Navigation from "../../../Components/Common/Navigation";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Copyright from "../../../Components/Copyright";
+import SymptomService from "../../../Services/SymptomService";
+import SymptomGraph from "../../../Components/Charts/SymptomGraph"
+import SymptomCounter from "../../../Components/Dashboard/Counters/SymptomCounter";
+
+describe('<SymptomTrends />', () => {
+    test('Renders a Navigation component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(Navigation).length).toBe(1);
+    });
+
+    test('Renders a Container component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(Container).length).toBe(1);
+    });
+
+    test('Renders a Grid component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(Grid).length).toBe(3);
+    });
+
+    test('Renders a Paper component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(Paper).length).toBe(2);
+    });
+
+    test('Renders a SymptomGraph component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(SymptomGraph).length).toBe(1);
+    });
+
+    test('Renders a SymptomCounter component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(SymptomCounter).length).toBe(1);
+    });
+
+    test('Renders a Box component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(Box).length).toBe(1);
+    });
+
+    test('Renders a Copyright component', () => {
+        const wrapper = shallow(<SymptomTrends />);
+        expect(wrapper.find(Copyright).length).toBe(1);
+    });
+});
